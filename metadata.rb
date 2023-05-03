d6541ba2b449f74393a661c1d80d7dda1b63046b
Dan Webb <dan.webb@damacus.io>
2023-05-03 09:44:57 +0100

    Fix metadata

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,14 @@ maintainer_email  'help@sous-chefs.org'
 license           'Apache-2.0'
 description       'Installs emacs'
 version           '0.11.5'
+source_url        'https://github.com/sous-chefs/emacs'
+issues_url        'https://github.com/sous-chefs/emacs/issues'
 
-%w(ubuntu debian redhat centos scientific fedora arch freebsd).each do |os|
-  supports os
-end
+supports 'ubuntu'
+supports 'debian'
+supports 'redhat'
+supports 'centos'
+supports 'scientific'
+supports 'fedora'
+supports 'arch'
+supports 'freebsd'
